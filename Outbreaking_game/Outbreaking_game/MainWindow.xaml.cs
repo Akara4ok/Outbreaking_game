2f625d4f5121e49641e290f52fbb5082069cc4ca
Mykyta <nikitochkaaaaaa@gmail.com>
2021-03-04 20:10:35 +0200

    First

--- Outbreaking_game/Outbreaking_game/MainWindow.xaml.cs
+++ Outbreaking_game/Outbreaking_game/MainWindow.xaml.cs
@@ -15,14 +15,20 @@ using System.Windows.Shapes;
 
 namespace Outbreaking_game
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = TextBox1.Text;
+            if (text != "")
+            {
+                MessageBox.Show(text);
+            }
         }
     }
 }
